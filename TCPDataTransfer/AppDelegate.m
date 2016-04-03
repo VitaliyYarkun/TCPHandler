6ccefdc22956d4a4de04a33c4e96687b3742caff
Vitaliy Yarkun <AdamFox@mbp-vitaliy.dlink>
2016-04-03 10:12:41 +0300

    Initial Commit

---
+++ TCPDataTransfer/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  TCPDataTransfer
+//
+//  Created by Vitaliy Yarkun on 4/3/16.
+//  Copyright © 2016 Vitaliy Yarkun. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ TCPDataTransfer/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  TCPDataTransfer
+//
+//  Created by Vitaliy Yarkun on 4/3/16.
+//  Copyright © 2016 Vitaliy Yarkun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
